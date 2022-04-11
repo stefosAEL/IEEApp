fbfc7bfd27d8a9888da62d0c5c7bf2bdd1473737
stefosAEL <45077043+stefosAEL@users.noreply.github.com>
2022-04-11 23:01:38 +0300

    Set up TableView rather than CollectionView

--- IEEApps/IEEApps/Model/MainModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MainModel.swift
-//  IEEApps
-//
-//  Created by Stefanos Kafkalias on 27/03/2022.
-//
-
-import Foundation
--- IEEApps/IEEApps/ViewControllers/MainViewController.swift
+++ IEEApps/IEEApps/ViewControllers/MainViewController.swift
@@ -9,30 +9,56 @@ import Foundation
 import UIKit
 import Alamofire
 
-class MainViewController : UIViewController {
-    
+class MainViewController : UIViewController,UITableViewDelegate, UITableViewDataSource{
+    let reuseIdentifier = "PublicAnnsCell"
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var LogInBtn: UIButton!
-    
     var publicAnns: [PublicAnn]?
-    
     override func viewDidLoad() {
-        LogInBtn.titleLabel?.font =  UIFont.systemFont(ofSize: 15)
-            
-        fetchPublicAnn()
         super.viewDidLoad()
-    }
-    
-    func fetchPublicAnn() {
+        LogInBtn.titleLabel?.font =  UIFont.systemFont(ofSize: 15)
+        tableView.dataSource = self
+        tableView.delegate = self
+//        self.collectionView.register(PublicAnnsCollectionViewCell.self, forCellWithReuseIdentifier: "PublicAnnsCell")
         DataContext.instance.getAnnouncemnets(completion: { [weak self] publicAnns in
             if let publicAnns = publicAnns {
                 self?.publicAnns = publicAnns.data
-                print(publicAnns.data[0].title)
-                print(publicAnns.data[0].body)
-                print(publicAnns.data[0].created_at)
-                print(publicAnns.data[0].tags[0].title)
-                print(publicAnns.data[0].author.name)
             }
+            self?.tableView.reloadData()
         })
+    
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let publicAnns = publicAnns
+        {
+           return publicAnns.count
+        }else {
+           return 0
+        }
+        }
+        // method to run when table view cell is tapped
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            // note that indexPath.section is used rather than indexPath.row
+            print("You tapped cell number \(indexPath.section).")
+        }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // create a new cell if needed or reuse an old one
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier , for: indexPath as IndexPath) as! PublicAnnTableViewCell
+        let announcement = publicAnns?[indexPath.row]
+        //cell.teacherLabel = announcement?.author.name  // set the text from the data model
+        cell.bodyLabel.text = announcement?.body
+        cell.dateTimeLabel.text = announcement?.created_at
+        cell.eventLabel.text = announcement?.tags[0].title
+        cell.titleLabel.text = announcement?.title
+        cell.event2Label.text=announcement?.tags[1].title
+        
+        
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 8
+        cell.clipsToBounds = true
+        return cell
     }
 
 }
